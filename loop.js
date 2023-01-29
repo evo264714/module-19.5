f7d47d9915dee13204d379c243dbe2ce16120c73
evo264714 <mh264714@gmail.com>
2023-01-29 16:35:14 +0600

    first commit

---
+++ loop.js
@@ -0,0 +1,95 @@
+//* 1: show "mon valo nei" 39 times
+// for(var i = 0; i < 39; i++){
+//     console.log('Mon valo nei');
+// }
+
+//* 2: show numbers between 58-98
+
+// for(var i = 58; i <= 98; i++){
+//     console.log(i);
+// }
+
+//* 3: even numbers between 412 to 456
+
+// for(var i = 412; i <= 456; i += 2){
+//     console.log(i);
+// }
+
+//* 4: show odd numbers between 581 to 623
+
+// for(var i = 581; i <= 623; i += 2){
+//     console.log(i)
+// }
+
+//* 5: declare an array and show them as output
+
+// var names = ['Messi', 'Ronaldo', 'Neymar', 'Mbappe'];
+
+// for(var i = 0; i < names.length; i++){
+//     console.log(names[i]);
+// }
+ //* 6: Create an array for all the mobile phones. Display all the elements of the array by using a while loop
+
+//  var phones = ['Nokia', 'Samsung', 'iPhone',
+// 'Xiaomi', 'Oneplus'];
+
+//  var i = 0;
+//  while(i < phones.length){
+//     console.log(phones[i]);
+//     i++;
+//  }
+
+//* 7: Run a loop from 30 to 86. this loop will stop if the values get higher than 44
+
+// var i = 30;
+
+// while(i <= 86){
+//     if(i > 44){
+//         break;
+//     }
+//     console.log(i);
+//     i++;
+// }
+
+// for (var i = 30; i <= 86; i++){
+//     if(i > 44){
+//         break;
+//     }
+//     console.log(i);
+// }
+
+//* 8: Write the price of the books that you have. Display the prices if the prices is lower than 200
+
+// var books = [890, 420, 500, 150, 180, 90, 360];
+
+// for(var i = 0; i < books.length; i++){
+//     if(books[i] < 200){
+//         console.log(books[i])
+
+//     }
+// }
+
+//* 9: Let's say you have x amount of money. if you have more than 80,000, then you will by a mac
+//* if more than 60,000 then you will by gaming laptop
+//* if you more than 40,000 then  you will by Lenovo Yoga
+//* if you have more than 20,000 then  you will by an used laptop
+//* otherwise, you will use you mobile phone
+
+// var money = 10000;
+
+// if(money >= 80000){
+//     console.log('Buy a Mac');
+// }
+// else if(money >= 60000){
+//     console.log('Buy a laptop');
+// }
+// else if(money >= 40000){
+//     console.log('Buy a Lenovo');
+// }
+// else if(money >= 20000){
+//     console.log('Buy a used laptop');
+// }
+// else{
+//     console.log('Using a phone')
+
+// }
